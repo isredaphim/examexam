bf209dc3afaff00e2a58c6ba0c4e40bb1ceb5fc9
isredaphim <55270578+isredaphim@users.noreply.github.com>
2020-02-12 17:35:43 +0800

    Update index2.php

--- index2.php
+++ index2.php
@@ -8,34 +8,33 @@ $domOBJ->load("https://www.bls.gov/feed/lau_latest.rss");
 
 <head>
     <meta charset="UTF-8">
-	
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=100%, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <title>Job Opening Survey</title>
 </head>
 
 <body>
-<button><link a href="index2.php"/>
     <div>
-<img src="doc.jpg" style="width:100%;height:25%" />
-
-<div class="container">
-
- <div class="centered">Job Opening and Labor Turnover Survey</div>
-</div>
-
-
+	    <img src="doc.jpg" style="width=100%;height=25%"/></br>
+        <button type="button">CLICK CLICK<link a href="index.php"/></button>
         <?php
-        $content = $domOBJ->getElementsByTagName("entry");
+      
+        $content = $domOBJ->getElementsByTagName("item");
 
         foreach ($content as $data) {
             $title = $data->getElementsByTagName("title")->item(0)->nodeValue;
-            $content = $data->getElementsByTagName("content")->item(0)->nodeValue;
+            $link = $data->getElementsByTagName("link")->item(0)->nodeValue;
+            $description = $data->getElementsByTagName("description")->item(0)->nodeValue;
+            $source = $data->getElementsByTagName("source")->item(0)->nodeValue;
+            $pubdate = $data->getElementsByTagName("pubDate")->item(0)->nodeValue;
         ?>
             <ul>
                 <li><strong><?php echo $title ?></strong></li>
                 <ul>
-                    <li><?php echo $content ?></li>
+                    <li><?php echo $link ?></li>
+                    <li><?php echo $description ?></li>
+                    <li><?php echo $source ?></li>
+                    <li><?php echo $pubdate ?></li>
                 </ul>
             </ul>
         <?php } ?>
